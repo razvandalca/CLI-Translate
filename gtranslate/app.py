16ddbf7c1588eda5cfd7cef0a177d98d227124f1
Razvan Dalca <dalca.razvan@gmail.com>
2021-02-16 22:01:45 +0200

    Add version in as environment variable

--- gtranslate/app.py
+++ gtranslate/app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from cliff.app import App
@@ -9,10 +10,9 @@ class TranslateApp(App):
     def __init__(self):
         super(TranslateApp, self).__init__(
             description='Google Translate CLI',
-            version='0.1',
+            version=os.environ.get('CLI_APP_VERSION', default='0.0'),
             command_manager=CommandManager('gtranslate.cli'),
-            deferred_help=True,
-            )
+        )
 
 
 def main(argv=sys.argv[1:]):
